6a6fd0269f578b834270cecb3d30a655afbbdfec
Shuvaranjan <shuvaranjans@gmail.com>
2026-09-16 22:08:06 +0530

    updated!

--- JS/app.js
+++ JS/app.js
@@ -1,40 +1,38 @@
 // Reset the form fields when the page loads
 window.onload = function () {
-    const frame = document.querySelector(".frame");
-    document.getElementById("contact-form")?.reset();
+  const frame = document.querySelector(".frame");
+  document.getElementById("contact-form")?.reset();
 
-    // Show animation
-    // frame.style.display = "block";
-    frame.style.transform = "translateY(0)";
+  // Show animation
+  // frame.style.display = "block";
+  frame.style.transform = "translateY(0)";
 
-    document.body.style.overflow = "hidden";
+  document.body.style.overflow = "hidden";
 
-    // Hide smoothly after 2 seconds
+  // Hide smoothly after 2 seconds
+  setTimeout(() => {
+    frame.style.transform = "translateY(-100%)";
+
+    // Wait until transition fully ends before hiding
     setTimeout(() => {
-        frame.style.transform = "translateY(-100%)";
-
-        // Wait until transition fully ends before hiding
-        setTimeout(() => {
-            document.body.style.overflow = "auto";
-            frame.style.display = "none";
-        }, 1500);
-    }, 2000);
+      document.body.style.overflow = "auto";
+      frame.style.display = "none";
+    }, 1500);
+  }, 2000);
 };
 
-
 // Work when something have on input field
-document.querySelectorAll(".input-field input, .textarea-field textarea")
-    .forEach(field => {
-        field.addEventListener("input", () => {
-            if (field.value.trim() !== "") {
-                field.classList.add("filled");
-            } else {
-                field.classList.remove("filled");
-            }
-        });
+document
+  .querySelectorAll(".input-field input, .textarea-field textarea")
+  .forEach((field) => {
+    field.addEventListener("input", () => {
+      if (field.value.trim() !== "") {
+        field.classList.add("filled");
+      } else {
+        field.classList.remove("filled");
+      }
     });
-
-
+  });
 
 const contactForm = document.getElementById("contact-form");
 const user_name = document.getElementById("userName");
@@ -49,120 +47,121 @@ let hideTimeout;
 const gmail_Format = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
 contactForm.addEventListener("submit", function (event) {
+  // Stop form first
+  event.preventDefault();
 
-    // Stop form first
-    event.preventDefault();
+  // Empty field check
+  if (
+    user_name.value.trim() == "" ||
+    user_message.value.trim() == "" ||
+    gmailInput.value.trim() == "" ||
+    (user_name.value.trim() == "" && user_message.value.trim() == "") ||
+    (user_name.value.trim() == "" && gmailInput.value.trim() == "") ||
+    (gmailInput.value.trim() == "" && user_message.value.trim() == "")
+  ) {
+    messageBox.style.right = "1rem";
+    message.innerHTML = "Please Fill the Empty Section";
 
-    // Empty field check
-    if (user_name.value.trim() == "" || user_message.value.trim() == "" || gmailInput.value.trim() == "" || user_name.value.trim() == "" && user_message.value.trim() == "" || user_name.value.trim() == "" && gmailInput.value.trim() == "" || gmailInput.value.trim() == "" && user_message.value.trim() == "") {
+    clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(() => {
+      hideMessage();
+    }, 5000);
 
-        messageBox.style.right = "1rem";
-        message.innerHTML = "Please Fill the Empty Section";
+    return;
+  }
 
-        clearTimeout(hideTimeout);
-        hideTimeout = setTimeout(() => {
-            hideMessage();
-        }, 5000);
+  // Gmail validation
+  if (!gmail_Format.test(gmailInput.value.trim())) {
+    messageBox.style.right = "1rem";
+    message.innerHTML = "Please enter a valid Gmail address!";
 
-        return;
+    clearTimeout(hideTimeout);
+    hideTimeout = setTimeout(() => {
+      hideMessage();
+    }, 5000);
+
+    return;
+  }
+
+  // Hide message function
+  function hideMessage() {
+    messageBox.style.right = "-100%";
+  }
+
+  // If valid → Submit Web3Forms
+  contactForm.submit();
+
+  // Reload page to reset form
+  window.addEventListener("pageshow", function (event) {
+    if (event.persisted) {
+      // If the page was cached and user returns, reload it
+      window.location.reload();
     }
-
-    // Gmail validation
-    if (!gmail_Format.test(gmailInput.value.trim())) {
-
-        messageBox.style.right = "1rem";
-        message.innerHTML = "Please enter a valid Gmail address!";
-
-        clearTimeout(hideTimeout);
-        hideTimeout = setTimeout(() => {
-            hideMessage();
-        }, 5000);
-
-        return;
-    }
-
-    // Hide message function
-    function hideMessage() {
-        messageBox.style.right = "-100%";
-    }
-
-    // If valid → Submit Web3Forms
-    contactForm.submit();
-
-
-    // Reload page to reset form
-    window.addEventListener("pageshow", function (event) {
-        if (event.persisted) {
-            // If the page was cached and user returns, reload it
-            window.location.reload();
-        }
-    });
-
+  });
 });
 
 contactForm.addEventListener("keydown", function (event) {
-    if (event.ctrlKey && event.key === "Enter") {
-        event.preventDefault();
-        contactForm.requestSubmit();
-    }
+  if (event.ctrlKey && event.key === "Enter") {
+    event.preventDefault();
+    contactForm.requestSubmit();
+  }
 });
 
 closeIcon.addEventListener("click", function () {
-    messageBox.style.right = "-100%";
+  messageBox.style.right = "-100%";
 });
-
 
 // Scroll Up Button
 const scrollUp = () => {
-    const scrollUp = document.getElementById('scroll-up')
+  const scrollUp = document.getElementById("scroll-up");
 
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scrollUp') : scrollUp.classList.remove('show-scrollUp');
-}
-window.addEventListener('scroll', scrollUp);
-
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scrollUp")
+    : scrollUp.classList.remove("show-scrollUp");
+};
+window.addEventListener("scroll", scrollUp);
 
 // Scroll Sections Active Link
-const sections = document.querySelectorAll('section[id]');
-
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-    const scrollDown = window.scrollY;
+  const scrollDown = window.scrollY;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav_list a[href*=' + sectionId + ']')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav_list a[href*=" + sectionId + "]",
+      );
 
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link');
-        } else {
-            sectionsClass.classList.remove('active-link');
-        }
-    })
-}
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
 
-window.addEventListener('scroll', scrollActive);
-
+window.addEventListener("scroll", scrollActive);
 
 // ScrollReveal Animation (When Scroll windows animate sections)
 const sReveal = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 350,
-    reset: 'true'
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 350,
+  reset: "true",
+});
 
-})
-
-sReveal.reveal('.profile, .contact_form');
-sReveal.reveal('.info', { origin: 'left', delay: 800 });
-sReveal.reveal('.skills', { origin: 'left', delay: 1000 });
-sReveal.reveal('.about', { origin: 'right', delay: 1200 });
-sReveal.reveal('.projects_card, .services_card, .experiance_card, .section-title', { interval: 100 });
-
-
-
+sReveal.reveal(".profile, .contact_form");
+sReveal.reveal(".info", { origin: "left", delay: 800 });
+sReveal.reveal(".skills", { origin: "left", delay: 1000 });
+sReveal.reveal(".about", { origin: "right", delay: 1200 });
+sReveal.reveal(
+  ".projects_card, .services_card, .experiance_card, .section-title",
+  { interval: 100 },
+);
 
 // GSAP Animation (When Scroll windows animate sections)
 // gsap.from("#projects",{
@@ -172,11 +171,11 @@ sReveal.reveal('.projects_card, .services_card, .experiance_card, .section-title
 //     duration:5,
 //     scrollTrigger:{
 //         trigger:"#projects",
-//         start:"top 50%",    
+//         start:"top 50%",
 //         end:"top 30%",
 //         scroller:"body",
 //         scrub:2,
-        
+
 //     }
 // })
 
@@ -186,7 +185,7 @@ sReveal.reveal('.projects_card, .services_card, .experiance_card, .section-title
 //     duration:2,
 //     scrollTrigger:{
 //         trigger:".services_container",
-//         start:"top 80%",    
+//         start:"top 80%",
 //         end:"top 60%",
 //         scroller:"body",
 //         scrub:1,
@@ -194,27 +193,25 @@ sReveal.reveal('.projects_card, .services_card, .experiance_card, .section-title
 //     }
 // })
 
-
-
 // gsap.from("#experiance",{
 //     y: 100,
 //     opacity:0,
 //     duration:3,
 //     scrollTrigger:{
 //         trigger:"#experiance",
-//         start:"top 50%",    
+//         start:"top 50%",
 //         end:"top 20%",
 //         scroller:"body",
 //         scrub:1,
-        
+
 //     }
 // })
 
-
-
-document.getElementById("contact-form").addKeyListener("keydown", function (event) {
+document
+  .getElementById("contact-form")
+  .addKeyListener("keydown", function (event) {
     if (event.key === "Enter" && !event.shiftKey) {
-        event.preventDefault(); // Prevent form submission on Enter key press
-        document.getElementById("contact-form").submit(); // Submit the form
+      event.preventDefault(); // Prevent form submission on Enter key press
+      document.getElementById("contact-form").submit(); // Submit the form
     }
-});
+  });
